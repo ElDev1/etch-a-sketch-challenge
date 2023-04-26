acc5c8a326c091861bc17207a360748fcf248ba4
Devi Amaolo <deviamaolo@gmail.com>
2023-04-26 15:51:40 -0300

    create buttons and range input state logic

---
+++ script.js
@@ -0,0 +1,31 @@
+//state variables
+let selected = 'Color mode';
+let range = 1;
+let color = '#A52A2A';
+
+//buttons
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        selected = button.textContent;
+        if(button.classList === 'selected') return;
+        button.classList.add("selected");
+
+        buttons.forEach(elem => {
+            if(button.classList !== elem.classList) {
+                elem.classList.remove('selected');
+            }
+        });
+    });
+});
+
+//inputs 
+const rangeInput = document.querySelector('.range-input');
+const rangeCounter = document.querySelector('.range-selector p');
+
+rangeInput.addEventListener('input', () => {
+    range = rangeInput.value
+    rangeCounter.textContent = `${range} x ${range}`
+    console.log(range)
+})
