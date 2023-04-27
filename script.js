37deab526420b1997d9af72e65a094e490644098
Devi Amaolo <deviamaolo@gmail.com>
2023-04-27 03:19:37 -0300

    create drawing tablet logic

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-//state variables
+// state variables
 let selected = 'Color mode';
 let range = 1;
 let color = '#A52A2A';
 
-//buttons
+// buttons
 const buttons = document.querySelectorAll('button');
 
 buttons.forEach(button => {
@@ -20,13 +20,25 @@ buttons.forEach(button => {
     });
 });
 
-//inputs 
+// inputs 
 const rangeInput = document.querySelector('.range-input');
 const rangeCounter = document.querySelector('.range-selector p');
 
 rangeInput.addEventListener('input', () => {
     range = rangeInput.value;
     rangeCounter.textContent = `${range} x ${range}`;
+    while(drawingTablet.hasChildNodes()) {
+        drawingTablet.removeChild(drawingTablet.firstChild)
+    }
+
+    for(let i = 0; i < range ** 2; i++) {
+        const pixel = document.createElement('div');
+        pixel.style.flexBasis = `calc(100% / ${range})`;
+        pixel.addEventListener('mousedown', () => {
+            pixel.style.backgroundColor = `${color}`;
+        })
+        drawingTablet.appendChild(pixel);
+    }
 })
 
 const colorPick = document.querySelector('.color-pick');
@@ -38,3 +50,11 @@ colorInput.addEventListener('input', () => {
 })
 
 
+// initial draw-table 
+const drawingTablet = document.querySelector('.drawing-tablet');
+const pixel = document.createElement('div');
+pixel.style.flexBasis = `calc(100% / ${range})`;
+pixel.addEventListener('mousedown', () => {
+    pixel.style.backgroundColor = `${color}`;
+})
+drawingTablet.appendChild(pixel)
